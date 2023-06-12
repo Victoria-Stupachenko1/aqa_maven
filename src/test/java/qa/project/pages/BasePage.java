a7a214811081ecfe5725735b8062213deba3a1a2
Victoria-Stupachenko1 <vviks11006@gmail.com>
2023-06-12 22:13:40 +0300

    first commit

---
+++ src/test/java/qa/project/pages/BasePage.java
@@ -0,0 +1,12 @@
+package qa.project.pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class BasePage {
+    public static SelenideElement cart = $(".header-actions__item--cart");
+
+    public static SelenideElement search = $("[name = 'search']");
+
+}
